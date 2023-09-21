7540849c0119d3c2356211baa210f467c2707c8f
gebi <michalnc96@gmail.com>
2023-09-21 08:05:56 +0200

    Zadanie domowe - dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var gender = "Kobieta";
+var age = 33;
+
+if (gender == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (gender == "Mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else;
+
+
